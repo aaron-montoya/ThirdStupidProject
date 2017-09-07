77f8de1e2d73b87c49caca487ae8ba9b6970ce5c
Aaron Montoya <aatoya7@gmail.com>
2017-09-07 13:53:03 -0600

    Learned about using a Scanner object.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,15 +1,23 @@
 package stupid.controller;
 
-/**
- * @author Aaron Montoya
- *
- */
+import java.util.Scanner;
+
 
 public class StupidController
 {
-
 	public void start()
 	{
 		System.out.println("I am Aaron Montoya");
+		
+		testScanner();
+	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println("What is your favorite TV show?");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh cool, you like " + answer);
 	}
 }
